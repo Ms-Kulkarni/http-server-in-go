42b4baf93c7d7f336c2caae033cbd144662e233e
Ms kulkarni <mskulkarnik2@gmail.com>
2024-11-24 01:59:13 +0530

    first commit

---
+++ .history/static/main_20241124012135.go
@@ -0,0 +1,46 @@
+package main
+
+import(
+"fmt"
+"log"
+"net/http"
+)
+
+func forHandler(w http.ResponseWriter, r *http.Request){
+	if err := r.ParderForm(); err != nil{
+		fmt.Fprintf(w, "ParserForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Post request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "address = %s\n", address)
+
+}
+
+func hellHandler(w http.ResponseWrite, r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	} 
+	if r.Method != "GET"{
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+
+
+}
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./static")) 
+	http.HandleFunc("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandlerFunc("/hello", helloHandler)
+
+	fmt.Prinf("strting server at port 8000\n")
+	if err := http:ListenAndServer(":8000", nil); err|=nil{
+		log.Fatal(err)
+	}
+} 
---
+++ go.mod
@@ -0,0 +1,3 @@
+module staic
+
+go 1.23.3
